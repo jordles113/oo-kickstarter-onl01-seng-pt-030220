523ae7ee15242a659147142a8a13446fce92d94a
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-30 21:08:28 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,13 @@
 class Backer
-  attr_reader :name 
- 
-  def intialize()
-    @name = name 
-  end 
-end 
+  attr_accessor :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project
+    project.add_backer(self) unless project.backers.include?(self)
+  end
+end
